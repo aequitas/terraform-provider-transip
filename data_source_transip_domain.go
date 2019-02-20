1a582547b9b136b1e6e36fa9688721a593e433ae
Johan Bloemberg <github@ijohan.nl>
2019-02-20 10:45:28 +0100

    add base provider, domain datasource

---
+++ data_source_transip_domain.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/transip/gotransip"
+	"github.com/transip/gotransip/domain"
+)
+
+func dataSourceDomain() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceDomainRead,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Required: true,
+				Type:     schema.TypeString,
+			},
+			"nameservers": {
+				Computed: true,
+				Type:     schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeMap,
+				},
+			},
+			"is_locked": {
+				Computed: true,
+				Type:     schema.TypeBool,
+			},
+			// "whoiscontact": {
+			// 	Computed: true,
+			// 	Type:     schema.TypeList,
+			// 	Elem: &schema.Schema{
+			// 		Type: schema.TypeMap,
+			// 	},
+			// },
+			// "registration_data": {
+			// 	Computed: true,
+			// 	Type:     schema.TypeString,
+			// },
+			// "renewal_date": {
+			// 	Computed: true,
+			// 	Type:     schema.TypeString,
+			// },
+		},
+	}
+}
+
+func dataSourceDomainRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(gotransip.Client)
+	name := d.Get("name").(string)
+
+	i, err := domain.GetInfo(client, name)
+	if err != nil {
+		return fmt.Errorf("failed to lookup domain %q: %s", name, err)
+	}
+
+	var nameservers []map[string]string
+	for _, n := range i.Nameservers {
+		nameservers = append(nameservers, map[string]string{
+			"hostname":     n.Hostname,
+			"ipv4_address": n.IPv4Address.String(),
+			"ipv6_address": n.IPv6Address.String(),
+		})
+	}
+
+	d.SetId(i.Name)
+	d.Set("is_locked", i.IsLocked)
+	d.Set("nameservers", nameservers)
+
+	return nil
+}
---
+++ provider.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/transip/gotransip"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"account_name": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Name of the Transip account.",
+			},
+			"private_key": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Contents of the private key file to be used to authenticate.",
+			},
+		},
+
+		ConfigureFunc: providerConfigure,
+
+		ResourcesMap: map[string]*schema.Resource{
+			"transip_dns_entry": resourceDNSEntry(),
+			"transip_domain":    resourceDomain(),
+		},
+
+		DataSourcesMap: map[string]*schema.Resource{
+			"transip_domain": dataSourceDomain(),
+		},
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	client, err := gotransip.NewSOAPClient(gotransip.ClientConfig{
+		AccountName:    d.Get("account_name").(string),
+		PrivateKeyBody: []byte(d.Get("private_key").(string)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
