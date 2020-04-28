133ab6d8413b83b86582de7e9c2d80d4495ffb57
Johan Bloemberg <github@ijohan.nl>
2020-04-28 20:11:12 +0200

    Migrate VPS resource.

--- resource_transip_vps.go
+++ resource_transip_vps.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
-
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/transip/gotransip/v5"
-	"github.com/transip/gotransip/v5/vps"
+	"github.com/transip/gotransip/v6"
+	"github.com/transip/gotransip/v6/product"
+	"github.com/transip/gotransip/v6/repository"
+	"github.com/transip/gotransip/v6/vps"
 )
 
 func resourceVps() *schema.Resource {
@@ -23,6 +25,11 @@ func resourceVps() *schema.Resource {
 				Type:     schema.TypeString,
 				ForceNew: true,
 			},
+			"description": {
+				Optional: true,
+				Type:     schema.TypeString,
+				ForceNew: true,
+			},
 			"product_name": {
 				Required: true,
 				Type:     schema.TypeString,
@@ -33,12 +40,18 @@ func resourceVps() *schema.Resource {
 				Type:     schema.TypeString,
 				ForceNew: true,
 			},
-			"description": {
+			"availability_zone": {
 				Optional: true,
+				Default:  "ams0",
 				Type:     schema.TypeString,
 				ForceNew: true,
 			},
-
+			"install_text": {
+				Optional: true,
+				Type:     schema.TypeString,
+				Default:  "",
+				ForceNew: true,
+			},
 			"disk_size": {
 				Computed: true,
 				Type:     schema.TypeInt,
@@ -47,7 +60,7 @@ func resourceVps() *schema.Resource {
 				Computed: true,
 				Type:     schema.TypeInt,
 			},
-			"processors": {
+			"cpus": {
 				Computed: true,
 				Type:     schema.TypeInt,
 			},
@@ -75,9 +88,12 @@ func resourceVps() *schema.Resource {
 				Computed: true,
 				Type:     schema.TypeBool,
 			},
-			"availability_zone": {
+			"tags": {
 				Computed: true,
-				Type:     schema.TypeString,
+				Type:     schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
 			},
 		},
 	}
@@ -85,18 +101,23 @@ func resourceVps() *schema.Resource {
 
 func resourceVpsCreate(d *schema.ResourceData, m interface{}) error {
 	name := d.Get("name").(string)
+
 	productName := d.Get("product_name").(string)
 	operatingSystem := d.Get("operating_system").(string)
+	availabilityZone := d.Get("availability_zone").(string)
 	addons := []string{}
+	InstallText := d.Get("install_text").(string)
 
-	client := m.(gotransip.Client)
+	client := m.(repository.Client)
+	repository := vps.Repository{Client: client}
 
-	availableProducts, err := vps.GetAvailableProducts(client)
+	productRepository := product.Repository{Client: client}
+	availableProducts, err := productRepository.GetAll()
 	if err != nil {
 		return fmt.Errorf("Failed to get available products: %s", err)
 	}
 	validProduct := false
-	for _, product := range availableProducts {
+	for _, product := range availableProducts.Vps {
 		if product.Name == productName {
 			validProduct = true
 		}
@@ -105,62 +126,68 @@ func resourceVpsCreate(d *schema.ResourceData, m interface{}) error {
 		return fmt.Errorf("Product %s is invalid. Valid product names are: %v", productName, availableProducts)
 	}
 
-	availableOss, err := vps.GetOperatingSystems(client)
-	if err != nil {
-		return fmt.Errorf("Failed to get available operating systems: %s", err)
-	}
-	validOs := false
-	for _, os := range availableOss {
-		if os.Name == operatingSystem {
-			validOs = true
-		}
-	}
-	if !validOs {
-		return fmt.Errorf("Product %s is invalid. Valid product names are: %v", productName, availableProducts)
+	base64InstallText := base64.StdEncoding.EncodeToString([]byte(InstallText))
+
+	vpsOrder := vps.Order{
+		ProductName:       productName,
+		OperatingSystem:   operatingSystem,
+		AvailabilityZone:  availabilityZone,
+		Hostname:          name,
+		Addons:            addons,
+		Base64InstallText: base64InstallText,
 	}
 
-	err = vps.OrderVps(client, productName, addons, operatingSystem, name)
+	err = repository.Order(vpsOrder)
 	if err != nil {
 		return fmt.Errorf("failed to order VPS %s: %s", name, err)
 
 	}
+
 	d.SetId(name)
+
+	d.Set("install_text", InstallText)
 
 	return resourceVpsRead(d, m)
 }
 
 func resourceVpsRead(d *schema.ResourceData, m interface{}) error {
-	client := m.(gotransip.Client)
-	name := d.Get("name").(string)
+	name := d.Id()
 
-	v, err := vps.GetVps(client, name)
+	client := m.(repository.Client)
+	repository := vps.Repository{Client: client}
+	v, err := repository.GetByName(name)
 	if err != nil {
-		return fmt.Errorf("failed to lookup VPS %q: %s", name, err)
+		return fmt.Errorf("failed to lookup vps %q: %s", name, err)
 	}
 
-	d.SetId(v.Name)
+	d.SetId(name)
 
+	d.Set("name", name)
 	d.Set("description", v.Description)
+	d.Set("product_name", v.ProductName)
 	d.Set("operating_system", v.OperatingSystem)
 	d.Set("disk_size", v.DiskSize)
 	d.Set("memory_size", v.MemorySize)
-	d.Set("processors", v.Processors)
+	d.Set("cpus", v.CPUs)
 	d.Set("status", v.Status)
-	d.Set("ipv4_address", v.IPv4Address.String())
-	d.Set("ipv6_address", v.IPv6Address.String())
-	d.Set("mac_address", v.MACAddress)
+	d.Set("ip_address", v.IPAddress)
+	d.Set("mac_address", v.MacAddress)
+	d.Set("is_locked", v.IsLocked)
 	d.Set("is_blocked", v.IsBlocked)
 	d.Set("is_customer_locked", v.IsCustomerLocked)
 	d.Set("availability_zone", v.AvailabilityZone)
+	d.Set("tags", v.Tags)
 
 	return nil
 }
 
 func resourceVpsDelete(d *schema.ResourceData, m interface{}) error {
-	client := m.(gotransip.Client)
 	name := d.Get("name").(string)
 
-	err := vps.CancelVps(client, name, gotransip.CancellationTimeImmediately)
+	client := m.(repository.Client)
+	repository := vps.Repository{Client: client}
+
+	err := repository.Cancel(name, gotransip.CancellationTimeImmediately)
 	if err != nil {
 		return fmt.Errorf("failed to cancel VPS %q: %s", name, err)
 	}
